5d2cf3514d7055bfb09c1ae9e5c4f9f6510b65d2
Afroja <afrojaafrinsammi00@gmail.com>
2025-02-13 16:03:50 +0600

    done

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = 5000
+
+
+// middlaware
+app.use(express.json())
+app.use(cors())
+const uri = "mongodb+srv://db_user:admin123123@cluster0.tcpvg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      const menuCollection = client.db('restaurant').collection('menu')
+      const reviewsCollection = client.db('restaurant').
+      collection('reviews')
+      const cartCollection = client.db('restaurant').
+      collection('carts')
+    //   menu collection get from database
+      app.get('/menu', async(req, res)=>{
+        const result = await menuCollection.find().toArray()
+        res.send(result)
+      })
+      app.get('/reviews', async(req, res)=>{
+        const result = await reviewsCollection.find().toArray()
+        res.send(result)
+      })
+      // order add to card api
+      app.post('/carts', async(req, res)=>{
+        const cart = req.body;
+        const result = await cartCollection.insertOne(cart)
+        res.send(result)
+      })
+      app.get('/carts', async(req, res)=>{
+        const email = req.query.email;
+        const query = {email:email}
+        const result = await cartCollection.find(query).toArray(express)
+        res.send(result)
+      })
+      app.delete("/carts/:id", async(req, res)=>{
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await cartCollection.deleteOne(query)
+        res.send(result)
+      })
+
+    } finally {
+      // Ensures that the client will close when you finish/error
+      // await client.close();
+    }
+  }
+  run().catch(console.dir);
+app.get('/', async(req, res)=>{
+    res.send('server is running....')
+})
+app.listen(port, ()=>{
+    console.log(`example app listening on ${port}`)
+})
